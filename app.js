43c8f3efd3ef233c05f78d5c39cbd71933f2c68b
Emil <emil_emil94@hotmail.com>
2018-08-22 22:18:49 +0200

    index.js file

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+const routes = require('./routes/api');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/api', routes);
+
 
 app.listen(port, () => {
   console.log('Listening on port 4000');
