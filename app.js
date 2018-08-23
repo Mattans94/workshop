d1e9122d1665a9767ad69e21d45462a24802072d
Unknown <marcusthelin97@gmail.com>
2018-08-23 13:37:08 +0200

    Added route so we can see our React app

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 const routes = require('./routes/api');
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/frontend/build')); // Serve static files from React build folder
 
 app.use('/api', routes);
 
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/frontend/build/index.html'); // Serve React's index.html file on all routes
+});
+
 
 app.listen(port, () => {
-  console.log('Listening on port 4000');
+  console.log(`Listening on port ${port}`);
 });
