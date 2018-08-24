812f7f6e4dfabf8ad523be1d5d8439c36b76fd10
Marcus Thelin <marcusthelin97@gmail.com>
2018-08-24 13:57:57 +0200

    app js change

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const port = process.env.PORT || 4000;
 const routes = require('./routes/api');
 const keys = require('./config/keys');
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb://${keys.mongoDB.username}:${keys.mongoDB.password}@ds225382.mlab.com:25382/workshop-react`, { useNewUrlParser: true })
+mongoose
+  .connect(
+    `mongodb://${keys.mongoDB.username}:${
+      keys.mongoDB.password
+    }@ds225382.mlab.com:25382/workshop-react`,
+    { useNewUrlParser: true }
+  )
   .then(() => {
     console.log('Mongoose connected');
   });
@@ -14,11 +20,10 @@ app.use(express.static(__dirname + '/frontend/build')); // Serve static files fr
 
 app.use('/api', routes);
 
-app.get('/', (req, res) => {
-  // res.sendFile(__dirname + '/frontend/build/index.html'); // Serve React's index.html file on all routes
-  res.sendFile(__dirname + '/routes/api/index.html'); // Serve React's index.html file on all routes
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/frontend/build/index.html'); // Serve React's index.html file on all routes
+  // res.sendFile(__dirname + '/routes/api/index.html'); // Serve React's index.html file on all routes
 });
-
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
