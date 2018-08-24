5eabfcebe59dbff1120db4c22cda5cd3744b37da
Emil <emil_emil94@hotmail.com>
2018-08-24 13:50:09 +0200

    gitflow-feature-stash: backend

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,21 +1,59 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const User = require('../../models/User');
 
 //home page
 router.post('/registration', (req, res, next) => {
-  // res.json(req.body);
-  res.send('HOME PAGE');
+  User.findOne({
+    email: req.body.email
+  })
+    .then((user) => {
+      if (user) {
+        res.json({
+          success: false,
+          msg: 'User already exists'
+        });
+      }
+      else {
+        new User({
+          email: req.body.email,
+          password: req.body.password,
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          age: req.body.age
+        }).save().then(newUser => {
+          res.json({
+            success: true,
+            user: newUser
+          })
+        });
+      }
+    })
 });
 
-router.get('/login', (req, res) => {
-
+router.post('/login', (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }).then((user) => {
+    if (!user) {
+      res.json({
+        success: false,
+        msg: 'User not found'
+      });
+    } else if (user.email === req.body.email && user.password === req.body.password) {
+      res.json(user);
+    } else {
+      res.json({
+        success: false,
+        msg: 'Invalid password'
+      });
+    }
+  });
 });
 
 router.post('/profile', (req, res) => {
   // HITTA ANVÄNDARE I DB
-  User.find()
-  res.json(user);
 });
 
 
