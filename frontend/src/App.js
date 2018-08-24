43a778b5f2a4238b0a799246fcc8e88d56ca833e
Marcus Thelin <marcusthelin97@gmail.com>
2018-08-24 13:52:44 +0200

    ZUUUUP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,80 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import Navigation from './components/Navigation';
+import Login from './components/Login';
+import Register from './components/Register';
 
 class App extends Component {
- 
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    // this.checkIfLoggedIn();
+  }
+
+  login = formData => {
+    this.setState({
+      user: formData
+    });
+  };
+
+  checkIfLoggedIn = () => {
+    const dummyData = {
+      email: 'dummy@email.com'
+    };
+    if (dummyData) {
+      this.setState({
+        user: dummyData
+      });
+    }
+  };
+
+  register = formData => {
+    fetch('/api/registration', {
+      method: 'POST',
+      body: formData
+    })
+      .then(res => res.json())
+      .then(res => this.setState(res))
+      .catch(err => console.log(err));
+  };
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
 
   render() {
     return (
       <Router>
         <div>
-          <p>Hej</p>
+          <Navigation user={this.state.user} />
+          {this.state.user ? (
+            <p>
+              You are:{' '}
+              {`${this.state.user.firstName} ${this.state.user.lastName}`}
+            </p>
+          ) : (
+            ''
+          )}
+          <div className="container">
+            <Switch>
+              <Route
+                path="/login"
+                exact
+                component={() => (
+                  <Login login={this.login} user={this.state.user} />
+                )}
+              />
+              <Route
+                path="/register"
+                exact
+                component={() => (
+                  <Register user={this.state.user} register={this.register} />
+                )}
+              />
+            </Switch>
+          </div>
         </div>
       </Router>
     );
